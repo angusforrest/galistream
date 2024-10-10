6773fa63245c6eded4b9ca047ecabf625ba65a26
Angus Forrest <contact@angusforrest.com>
2024-10-10 20:12:07 +1300

    spatial map trial

--- src/galistream/poster/spatial_map.py
+++ src/galistream/poster/spatial_map.py
@@ -6,6 +6,7 @@ import pickle
 import os
 
 def plot_scatter(orbits,gmc):
+	ts = numpy.array(orbits.t[numpy.arange(0,len(orbits.t),4)])
     COL3 = '#1E9CBF'
     COL2 = '#1EBF74'
     COL1 = '#BEA61E'
@@ -13,24 +14,23 @@ def plot_scatter(orbits,gmc):
     COLWHITE = '#F7F7F8'
     COLBLACK = '#001134'
     plt.ioff()
-    ax = plt.axes(projection='3d',computed_zorder=False,figsize=(12.56,29.7))
+    fig = plt.figure(figsize=(15.6,25.2))
+    ax = fig.add_subplot(1, 2, 1, projection='3d',computed_zorder=False)
     ax.grid(False)
-    offset = 0
-    ts = np.array([300,320,340,360])-offset
-    for t in ts:
-	    ax.set_xticks([])
-		ax.set_yticks([])
-		ax.set_zticks([])
-	    ax.set_xlabel(f"x (kpc)")
-	    ax.set_ylabel(f"y (kpc)")
-	    iso = numpy.array([orbits.x(t),orbits.y(t),orbits.z(t)])
-	    select = orbits.z(t) > 0
-	    ax.scatter(orbits.x(t)[select],orbits.y(t)[select],orbits.z(t)[select],s=1,zorder=1,color='#aaa')
-	    ax.scatter(gmc.x(t),gmc.y(t),gmc.z(t),zorder=0,s=1,color='#fff')
-	    ax.scatter(orbits.x(t)[numpy.logical_not(select)],orbits.y(t)[numpy.logical_not(select)],orbits.z(t)[numpy.logical_not(select)],s=1,zorder=-1,color='#aaa')
-	    ax.set_xlim(-10,10)
-	    ax.set_ylim(-10,10)
-	    ax.set_zlim(-1,1)
+    ax.set_xticks([])
+	ax.set_yticks([])
+	ax.set_zticks([])
+    ax.set_xlabel(f"x (kpc)")
+    ax.set_ylabel(f"y (kpc)")
+    t = ts[320]
+    iso = numpy.array([orbits.x(t),orbits.y(t),orbits.z(t)])
+    select = orbits.z(t) > 0
+    ax.scatter(orbits.x(t)[select],orbits.y(t)[select],orbits.z(t)[select],s=1,zorder=1,color='#aaa')
+    ax.scatter(gmc.x(t),gmc.y(t),gmc.z(t),zorder=0,s=1,color='#fff')
+    ax.scatter(orbits.x(t)[numpy.logical_not(select)],orbits.y(t)[numpy.logical_not(select)],orbits.z(t)[numpy.logical_not(select)],s=1,zorder=-1,color='#aaa')
+    ax.set_xlim(-10,10)
+    ax.set_ylim(-10,10)
+    ax.set_zlim(-1,1)
     plt.savefig("spatial_map.png",dpi=600,transparent=True)
     plt.savefig("spatial_map.svg",dpi=600,transparent=True)
     plt.close()
