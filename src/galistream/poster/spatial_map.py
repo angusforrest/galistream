698827c2cf7cc4c3aac9ce13b139845deec3ea28
Angus Forrest <contact@angusforrest.com>
2024-10-10 20:40:51 +1300

    added frame option

--- src/galistream/poster/spatial_map.py
+++ src/galistream/poster/spatial_map.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import pickle
 import os
 
-def plot_scatter(orbits,gmc):
+def plot_scatter(orbits,gmc,frame):
 	ts = numpy.array(orbits.t[numpy.arange(0,len(orbits.t),4)])
 	COL3 = '#1E9CBF'
 	COL2 = '#1EBF74'
@@ -14,69 +14,33 @@ def plot_scatter(orbits,gmc):
 	COLWHITE = '#F7F7F8'
 	COLBLACK = '#001134'
 	plt.ioff()
-	fig = plt.figure(figsize=(15.6,25.2))
-	ax = fig.add_subplot(3, 1, 1, projection='3d',computed_zorder=False)
+    fig,ax = plt.axes(projection='3d',computed_zorder=False)
 	ax.grid(False)
 	ax.set_xticks([])
 	ax.set_yticks([])
 	ax.set_zticks([])
 	ax.set_axis_off()
-	t = ts[360]
+	t = ts[frame]
 	iso = numpy.array([orbits.x(t),orbits.y(t),orbits.z(t)])
 	select = orbits.z(t) > 0
 	ax.scatter(orbits.x(t)[select],orbits.y(t)[select],orbits.z(t)[select],s=1,zorder=1,color=COL3)
 	ax.scatter(gmc.x(t),gmc.y(t),gmc.z(t),zorder=0,s=1,color=COL2)
-	ax.scatter(orbits.x(t)[numpy.logical_not(select)],orbits.y(t)[numpy.logical_not(select)],orbits.z(t)[numpy.logical_not(select)],s=1,zorder=-1,color=COL3)
+	ax.scatter(orbits.x(t)[numpy.logical_not(select)],orbits.y(t)[numpy.logical_not(select)],orbits.z(t)[numpy.logical_not(select)],s=1,zorder=-1,color=COL1)
 	ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
 	ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
 	ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-	ax.set_xlim(-10,10)
-	ax.set_ylim(-10,10)
+	ax.set_xlim(-9,9)
+	ax.set_ylim(-9,9)
 	ax.set_zlim(-1,1)
-	ax = fig.add_subplot(3, 1, 2, projection='3d',computed_zorder=False)
-	ax.grid(False)
-	ax.set_xticks([])
-	ax.set_yticks([])
-	ax.set_zticks([])
-	ax.set_axis_off()
-	t = ts[380]
-	iso = numpy.array([orbits.x(t),orbits.y(t),orbits.z(t)])
-	select = orbits.z(t) > 0
-	ax.scatter(orbits.x(t)[select],orbits.y(t)[select],orbits.z(t)[select],s=1,zorder=1,color=COL3)
-	ax.scatter(gmc.x(t),gmc.y(t),gmc.z(t),zorder=0,s=1,color=COL2)
-	ax.scatter(orbits.x(t)[numpy.logical_not(select)],orbits.y(t)[numpy.logical_not(select)],orbits.z(t)[numpy.logical_not(select)],s=1,zorder=-1,color=COL3)
-	ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-	ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-	ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-	ax.set_xlim(-10,10)
-	ax.set_ylim(-10,10)
-	ax.set_zlim(-1,1)
-	ax = fig.add_subplot(3, 1, 3, projection='3d',computed_zorder=False)
-	ax.grid(False)
-	ax.set_xticks([])
-	ax.set_yticks([])
-	ax.set_zticks([])
-	ax.set_axis_off()
-	t = ts[380]
-	iso = numpy.array([orbits.x(t),orbits.y(t),orbits.z(t)])
-	select = orbits.z(t) > 0
-	ax.scatter(orbits.x(t)[select],orbits.y(t)[select],orbits.z(t)[select],s=1,zorder=1,color=COL3)
-	ax.scatter(gmc.x(t),gmc.y(t),gmc.z(t),zorder=0,s=1,color=COL2)
-	ax.scatter(orbits.x(t)[numpy.logical_not(select)],orbits.y(t)[numpy.logical_not(select)],orbits.z(t)[numpy.logical_not(select)],s=1,zorder=-1,color=COL3)
-	ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-	ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-	ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-	ax.set_xlim(-10,10)
-	ax.set_ylim(-10,10)
-	ax.set_zlim(-1,1)
-	plt.savefig("spatial_map.png",dpi=600,transparent=True)
-	plt.savefig("spatial_map.svg",dpi=600,transparent=True)
+	plt.savefig(f"spatial_map_{frame}.png",dpi=600,transparent=True)
+	plt.savefig(f"spatial_map_{frame}.svg",dpi=600,transparent=True)
 	plt.close()
 
 def main():
 	parser = argparse.ArgumentParser("3d_plot")
 	parser.add_argument("-g","--gmc",default=False)
 	parser.add_argument("filename")
+	parser.add_argument("frame")
 	args =parser.parse_args()
 	if os.path.exists(args.filename):
 		with open(args.filename, "rb") as file:
@@ -90,7 +54,7 @@ def main():
 	else:
 		print("file provided does not exist")
 		return 0
-	plot_scatter(data,gmc)
+	plot_scatter(data,gmc,int(args.frame))
 	return 0
 
 if __name__ == '__main__':
