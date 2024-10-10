f2672e0b81429d53b72fae907771c3c934c061f5
Angus Forrest <contact@angusforrest.com>
2024-10-10 23:03:34 +1300

    alter gmc to be correct color in spatial map

--- src/galistream/poster/spatial_map.py
+++ src/galistream/poster/spatial_map.py
@@ -24,8 +24,8 @@ def plot_scatter(orbits,gmc,frame):
 	iso = numpy.array([orbits.x(t),orbits.y(t),orbits.z(t)])
 	select = orbits.z(t) > 0
 	ax.scatter(orbits.x(t)[select],orbits.y(t)[select],orbits.z(t)[select],s=1,zorder=1,color=COL3,depthshade=False)
-	ax.scatter(gmc.x(t),gmc.y(t),gmc.z(t),zorder=0,s=1,color=COL2,depthshade=False)
-	ax.scatter(orbits.x(t)[numpy.logical_not(select)],orbits.y(t)[numpy.logical_not(select)],orbits.z(t)[numpy.logical_not(select)],depthshade=False,s=1,zorder=-1,color=COL1)
+	ax.scatter(gmc.x(t),gmc.y(t),gmc.z(t),zorder=0,s=1,color=COL1,depthshade=False)
+	ax.scatter(orbits.x(t)[numpy.logical_not(select)],orbits.y(t)[numpy.logical_not(select)],orbits.z(t)[numpy.logical_not(select)],depthshade=False,s=1,zorder=-1,color=COL3)
 	ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
 	ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
 	ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
