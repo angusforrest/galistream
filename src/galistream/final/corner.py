d8c9afd22959bbbe7f9f68f65e07a5c3b4387e9d
Angus Forrest <contact@angusforrest.com>
2024-10-12 12:36:01 +1300

    triage

--- src/galistream/final/corner.py
+++ src/galistream/final/corner.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy
 import os
 import pickle
-import corner
+from corner import corner
 
 
 def plot(data,res):
@@ -13,7 +13,8 @@ def plot(data,res):
     energy = data[0,:,-1]
     momentum = data[1,:,-1]
     samples = numpy.array([(x,y-z) for x,y,z in zip(momentum,energy,l_ys)])
-    figure = corner.corner(samples) #,labels=[r"$L_z$",r"\Delta E"],quantiles=[0.16,0.5,0.84], show_titles=True,title_kwargs={"fontsize": 12}) 
+    figure = corner(samples) 
+    #,labels=[r"$L_z$",r"\Delta E"],quantiles=[0.16,0.5,0.84], show_titles=True,title_kwargs={"fontsize": 12}) 
     plt.savefig("figure_corner.eps",dpi=600)
     plt.savefig("figure_corner.png",dpi=600)
     plt.close()
