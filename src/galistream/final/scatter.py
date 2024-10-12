7b2207bf5a4a03f4467ced2337fcf22eea68da02
Angus Forrest <contact@angusforrest.com>
2024-10-12 21:03:36 +1300

    ahh 1d subplots are 1 index for some reason

--- src/galistream/final/scatter.py
+++ src/galistream/final/scatter.py
@@ -8,15 +8,15 @@ def plot(momentum,result):
     plt.rcParams['text.usetex'] = True
     plt.ioff()
     props = dict(boxstyle='round', facecolor='wheat', alpha=1)
-    fig,ax = plt.subplots(2,1)
+    fig,ax = plt.subplots(1,2)
     lz = momentum[1,:,-1]
     energy = momentum[0,:,-1]
-    ax[0,0].scatter(lz,energy)
-    ax[0,0].set_xlabel(r"$L_z$ (kpc km s${}^{-1}$)")
-    ax[0,0].set_ylabel(r"$\Delta E$ (km^2 s${}^{-2}$)")
-    ax[0,1].scatter(momentum[1,:,-1],momentum[0,:,-1]-result[0][-1])
-    ax[0,1].set_xlabel(r"$L_z$ (kpc km s${}^{-1}$)")
-    ax[0,1].set_ylabel(r"$\Delta E$ (km^2 s${}^{-2}$)")
+    ax[0].scatter(lz,energy)
+    ax[0].set_xlabel(r"$L_z$ (kpc km s${}^{-1}$)")
+    ax[0].set_ylabel(r"$\Delta E$ (km^2 s${}^{-2}$)")
+    ax[1].scatter(momentum[1,:,-1],momentum[0,:,-1]-result[0][-1])
+    ax[1].set_xlabel(r"$L_z$ (kpc km s${}^{-1}$)")
+    ax[1].set_ylabel(r"$\Delta E$ (km^2 s${}^{-2}$)")
     plt.savefig("figure_scatter.eps",dpi=300)
     plt.savefig("figure_scatter.png",dpi=300)
     plt.close()
