47bb73d794ee060e647f6f5fe9dae192ee80762e
Angus Forrest <contact@angusforrest.com>
2024-10-17 09:46:45 +1300

    fix?

--- src/galistream/final/scatter.py
+++ src/galistream/final/scatter.py
@@ -43,7 +43,9 @@ def plot(momentum,result):
     ax[1].axhline(per50,50.0,color="orange")
     ax[1].set_yticks([])
     ax[1].set_xticks([])
-    pertext=f"16th percentile {per16}\\50th percentile {per50}\\84th percentile {per84}"
+    pertext=f"16th percentile {per16:2.1f}\
+            50th percentile {per50:2.1f}\
+            84th percentile {per84:2.1f}"
     ax[1].text(0.70, 0.95, pertext, transform=ax[1].transAxes, fontsize=10, va='bottom', ha='left')
     plt.savefig("figure_histogram.png",dpi=600)
     plt.close()
