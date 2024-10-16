8c6f1a0b6a9fe212f1d634e5bd50b1fb491a0323
Angus Forrest <contact@angusforrest.com>
2024-10-16 21:10:43 +1300

    see if removing equal aspect fixes things

--- src/galistream/final/gmc_angular.py
+++ src/galistream/final/gmc_angular.py
@@ -15,7 +15,6 @@ def plot(orbits):
     ax[0,0].set_xlim(-10,10)
     ax[0,0].set_ylim(-10,10)
     ax[0,0].set_ylabel(f"y (kpc)")
-    ax[0,0].set_aspect('equal')
     ax[0,0].text(0.05, 0.95, r't = 0 Gyr', transform=ax[0,0].transAxes, fontsize=6,verticalalignment='top', bbox=props)
     ax[0,0].text(0.05, 0.05, 'A', transform=ax[0,0].transAxes, fontsize=10, fontweight='bold', va='bottom', ha='left')
     # 1 Gyr GMC Plot
@@ -24,7 +23,6 @@ def plot(orbits):
     ax[1,0].set_ylim(-10,10)
     ax[1,0].set_ylabel(f"y (kpc)")
     ax[1,0].set_xlabel(f"x (kpc)")
-    ax[1,0].set_aspect('equal')
     ax[1,0].text(0.05, 0.95, r't = 1 Gyr', transform=ax[1,0].transAxes, fontsize=6,
         verticalalignment='top', bbox=props)
     ax[1,0].text(0.05, 0.05, 'B', transform=ax[1,0].transAxes, fontsize=10, fontweight='bold', va='bottom', ha='left')
@@ -36,7 +34,6 @@ def plot(orbits):
     cbar.ax.set_yticklabels([r'-180', '0', r'180'])
     ax[0,1].set_xlim(-10,10)
     ax[0,1].set_ylim(-10,10)
-    ax[0,1].set_aspect('equal')
     ax[0,1].text(0.05, 0.95, r't = 0 Gyr', transform=ax[0,1].transAxes, fontsize=6,
         verticalalignment='top', bbox=props)
     ax[0,1].text(0.05, 0.05, 'C', transform=ax[0,1].transAxes, fontsize=10, fontweight='bold', va='bottom', ha='left')
