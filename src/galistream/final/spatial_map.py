a4370575232b753694963a57413cfd77b874e8d1
Angus Forrest <contact@angusforrest.com>
2024-10-16 21:13:53 +1300

    spatial aspect equal

--- src/galistream/final/spatial_map.py
+++ src/galistream/final/spatial_map.py
@@ -33,7 +33,7 @@ def plot_scatter(orbits,gmc):
 	ax.set_xlabel("x (kpc)")
 	ax.set_ylabel("y (kpc)")
 	ax.set_zlabel("z (kpc)")
-	ax.set_box_aspect((9, 9, 1))
+	ax.set_aspect('equal')
 	ax = fig.add_subplot(3,1, 2, projection='3d',computed_zorder=False)
 	ax.grid(False)
 	t = ts[380]
@@ -52,7 +52,9 @@ def plot_scatter(orbits,gmc):
 	ax.set_ylabel("y (kpc)")
 	ax.set_zlabel("z (kpc)")
 	ax.pbaspect = [2.0, 0.6, 0.25]
+	ax.set_aspect('equal')
 	ax = fig.add_subplot(3,1, 3, projection='3d',computed_zorder=False)
+	ax.set_aspect('equal')
 	ax.grid(False)
 	t = ts[400]
 	iso = numpy.array([orbits.x(t),orbits.y(t),orbits.z(t)])
