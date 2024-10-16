4fa0d515d6c7d53a70b29d78feaf6d3c63cfedef
Angus Forrest <contact@angusforrest.com>
2024-10-16 22:31:56 +1300

    update color and size

--- src/galistream/final/science.py
+++ src/galistream/final/science.py
@@ -5,25 +5,31 @@ import os
 import pickle
 
 def plot(d0_0,d1250_0,d1250_1,d1250_2,d2500_0,d2500_1,d2500_2,d5000_0,d5000_1):
+    COL3 = '#FFBA08'
+    COL2 = '#F48C06'
+    COL1 = '#DC2F02'
+    COL0 = '#9D0208'
+    COLWHITE = '#F7F7F8'
+    COLBLACK = '#001134'
     plt.rcParams['text.usetex'] = True
     plt.ioff()
-    fig,ax = plt.subplots(2,1,sharex=True)
+    fig,ax = plt.subplots(2,1,figsize=(8,10),sharex=True)
     fig.set_tight_layout(True)
     ts = d1250_0[-1]
-    ax[0].plot(ts,d0_0[1],c="blue",label="0",linestyle="dashed")
-    ax[0].plot(ts,d1250_0[1],c="blue",label="1250")
-    ax[0].plot(ts,d1250_1[1],c="blue")
-    ax[0].plot(ts,d1250_2[1],c="blue")
-    ax[0].plot(ts,d2500_0[1],c="green",label="2500")
-    ax[0].plot(ts,d2500_1[1],c="green")
-    ax[0].plot(ts,d2500_2[1],c="green")
-    ax[0].plot(ts,d5000_0[1],c="red",label="5000")
-    ax[0].plot(ts,d5000_1[1],c="red")
+    ax[0].plot(ts,d0_0[1],c=COL0,label="0",linestyle="dashed")
+    ax[0].plot(ts,d1250_0[1],c=COL1,label="1250")
+    ax[0].plot(ts,d1250_1[1],c=COL1)
+    ax[0].plot(ts,d1250_2[1],c=COL1)
+    ax[0].plot(ts,d2500_0[1],c=COL2,label="2500")
+    ax[0].plot(ts,d2500_1[1],c=COL2)
+    ax[0].plot(ts,d2500_2[1],c=COL2)
+    ax[0].plot(ts,d5000_0[1],c=COL3,label="5000")
+    ax[0].plot(ts,d5000_1[1],c=COL3)
     ax[0].legend(loc="upper left")
     ax[0].set_xlabel("time (Gyr)")
     ax[0].set_ylabel(r'$\sigma^2 = \Delta E$')
     ax[0].set_xlim(0,1)
-    ts = d1250_0[-1]
+    ax[0].legend(fontsize=20,loc="upper left",title="no. GMCs")
     bbox = dict(boxstyle='round', fc='blanchedalmond', ec='orange', alpha=1)
     ax[1].axvline(ts[12],0,0.95,linestyle='dashed')
     ax[1].text(ts[12],6.5,"A",bbox=bbox,horizontalalignment='center')
@@ -37,20 +43,20 @@ def plot(d0_0,d1250_0,d1250_1,d1250_2,d2500_0,d2500_1,d2500_2,d5000_0,d5000_1):
     ax[1].text(ts[33],6.5,"E",bbox=bbox,horizontalalignment='center')
     ax[1].axvline(ts[39],0,0.95,linestyle='dashed')
     ax[1].text(ts[39],6.5,"F",bbox=bbox,horizontalalignment='center')
-    ax[1].plot(ts,numpy.sqrt(d0_0[1]),c="blue",label="0",linestyle="dashed")
-    ax[1].plot(ts,numpy.sqrt(d1250_0[1]),c="blue",label="1250")
-    ax[1].plot(ts,numpy.sqrt(d1250_1[1]),c="blue")
-    ax[1].plot(ts,numpy.sqrt(d1250_2[1]),c="blue")
-    ax[1].plot(ts,numpy.sqrt(d2500_0[1]),c="green",label="2500")
-    ax[1].plot(ts,numpy.sqrt(d2500_1[1]),c="green")
-    ax[1].plot(ts,numpy.sqrt(d2500_2[1]),c="green")
-    ax[1].plot(ts,numpy.sqrt(d5000_0[1]),c="red",label="5000")
-    ax[1].plot(ts,numpy.sqrt(d5000_1[1]),c="red")
+    ax[1].plot(ts,numpy.sqrt(d0_0[1]),c=COL0,label="0",linestyle="dashed")
+    ax[1].plot(ts,numpy.sqrt(d1250_0[1]),c=COL1,label="1250")
+    ax[1].plot(ts,numpy.sqrt(d1250_1[1]),c=COL1)
+    ax[1].plot(ts,numpy.sqrt(d1250_2[1]),c=COL1)
+    ax[1].plot(ts,numpy.sqrt(d2500_0[1]),c=COL2,label="2500")
+    ax[1].plot(ts,numpy.sqrt(d2500_1[1]),c=COL2)
+    ax[1].plot(ts,numpy.sqrt(d2500_2[1]),c=COL2)
+    ax[1].plot(ts,numpy.sqrt(d5000_0[1]),c=COL3,label="5000")
+    ax[1].plot(ts,numpy.sqrt(d5000_1[1]),c=COL3)
     ax[1].legend(loc="upper left")
+    ax[1].legend(fontsize=20,loc="upper left",title="no. GMCs")
     ax[1].set_xlabel("time (Gyr)")
     ax[1].set_ylabel(r'$\sigma = \sqrt{\Delta E}$')
     ax[1].set_xlim(0,1)
-    plt.savefig("figure_sigma.eps",dpi=600)
     plt.savefig("figure_sigma.png",dpi=600)
     plt.close()
 
